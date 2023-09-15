1e3ee6f5ee2dc8f8d037385e54c46c95ad411a6f
guntekhunter <guntek2000@gmail.com>
2023-09-15 17:43:19 +0800

    fix: parking-place page interface

--- frontend/parking-reservation/components/cardPlace.vue
+++ frontend/parking-reservation/components/cardPlace.vue
@@ -1,12 +1,14 @@
 <template>
   <NuxtLink :to="'parking-places/' + id" class="p-5 shadow-md rounded-md">
     <div class="flex space-x-[1rem] items-center">
-      <div class="w-[40%]"><img src="~/assets/image/1.jpg" alt="" /></div>
+      <div class="w-[40%] bg-gray-200">
+        <img src="~/assets/image/1.jpg" alt="" />
+      </div>
       <div class="space-y-2">
         <div class="font-bold text-[1rem]">{{ name }}</div>
         <div class="text-[.8rem] text-gray-400">{{ location }}</div>
         <div class="text-[.8rem] text-gray-400">{{ capacity }} Vehicles</div>
-        <div class="text-[.8rem]">{{ amount }}</div>
+        <div class="text-[.8rem]">{{ amount }}/hour</div>
       </div>
     </div>
   </NuxtLink>
