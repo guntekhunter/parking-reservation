c4fa37db8a03a160e403a0f4d376c8e931ccf01d
guntekhunter <guntek2000@gmail.com>
2023-09-15 09:58:07 +0800

    feat: layouting the parking reservation card

---
+++ frontend/parking-reservation/components/card.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    :class="{ 'bg-blue-200': show, 'bg-green-200': !show }"
+    class="rounded-md shadow-md p-5"
+  >
+    <div>{{ name }}</div>
+    <div>{{ location }}</div>
+    <div>{{ capacity }}</div>
+    <div>{{ isAvailable }}</div>
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps } from "vue";
+
+const { name, location, capacity, isAvailable } = defineProps([
+  "name",
+  "location",
+  "capacity",
+  "isAvailable",
+]);
+
+let show = false;
+
+if (isAvailable === "available") {
+  show = true;
+}
+console.log(show);
+</script>
--- frontend/parking-reservation/pages/parking-spots/index.vue
+++ frontend/parking-reservation/pages/parking-spots/index.vue
@@ -1,10 +1,43 @@
 <template>
-  <div class="w-full bg-red-200">
-    <h1>ini daftar parking spots</h1>
+  <div class="w-full justify-center flex">
+    <div class="w-[80%] py-5">
+      <div class="grid grid-cols-4 grid-flow-col gap-4">
+        <Card
+          name="C1"
+          location="malakaji"
+          capacity="200"
+          isAvailable="available"
+          >ahhay</Card
+        >
+        <Card
+          name="C2"
+          location="malakaji"
+          capacity="200"
+          isAvailable="available"
+          >ahhay</Card
+        >
+        <Card
+          name="C3"
+          location="malakaji"
+          capacity="200"
+          isAvailable="available"
+          >ahhay</Card
+        >
+        <Card
+          name="C4"
+          location="malakaji"
+          capacity="200"
+          isAvailable="not available"
+          >ahhay</Card
+        >
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import Card from "../../components/card.vue";
+
 definePageMeta({
   layout: "default",
 });
