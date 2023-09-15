1de80e1d847df46e6cdfb9155324e9af16084c94
guntekhunter <guntek2000@gmail.com>
2023-09-15 17:41:29 +0800

    fix: parking spots interface

--- frontend/parking-reservation/components/card.vue
+++ frontend/parking-reservation/components/card.vue
@@ -1,9 +1,25 @@
 <template>
   <div
-    :class="{ 'hover:bg-gray-200': show, 'bg-gray-100 opacity-70': !show }"
-    class="p-5 duration-200 border-dashed border-t-2"
+    :class="{ 'hover:bg-gray-200': show, 'bg-gray-100': !show }"
+    class="p-5 duration-200 border-dashed border-t-2 h-[13rem]"
   >
-    <div class="bg-gray-400 p-y-[4rem] h-[10rem] items-center flex">
+    <div
+      :class="{ hidden: show, '': !show }"
+      class="h-full p-[.5srem] space-y-4"
+    >
+      <div
+        class="bg-green-200 border-green-400 border border-1 rounded-md py-2 px-2 text-[.8rem] text-green-400 flex justify-center"
+      >
+        Yours
+      </div>
+      <div class="flex justify-center">
+        <img src="~/assets/image/car.png" alt="" class="w-[4rem]" />
+      </div>
+    </div>
+    <div
+      class="p-y-[4rem] h-[10rem] items-center flex"
+      :class="{ hidden: !show, '': show }"
+    >
       <div class="w-full flex justify-center text-[1.5rem] font-bold">
         {{ name }}
       </div>
--- frontend/parking-reservation/components/modal/modalConfirm.vue
+++ frontend/parking-reservation/components/modal/modalConfirm.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="absolute inset-0 overflow-y-hidden bg-black bg-opacity-50 z-10">
+  <div
+    class="absolute inset-0 overflow-y-hidden bg-black bg-opacity-50 z-10 h-screen"
+  >
     <div class="flex items-start justify-center min-h-screen mt-24">
       <div class="rounded-md py-[2rem] px-[3rem] bg-white shadow-md">
         Payment Confirmed
